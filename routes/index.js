3158ef0be0dc40f116bff5d7d7cef3aaa5f1abb7
Echeverrias <juanan.echeve@gmail.com>
2015-07-13 21:00:03 +0200

    Primera pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
-' Hay que importar el módulo express explicitamente donde lo vayamos a utilizar, aunque ya esté importado en otro módulo
+//Hay que importar el módulo express explicitamente donde lo vayamos a utilizar, aunque ya esté importado en otro módulo
 var express = require('express');
 var router = express.Router();
+
+var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
+
+router.get('/quizes/question', quizController.question);
+router.get('/quizes/answer', quizController.answer);
 
 module.exports = router;
