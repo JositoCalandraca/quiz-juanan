942fc165aaaeeb1af8e9863b34c930f300194b49
Echeverrias <juanan.echeve@gmail.com>
2015-08-03 16:56:50 +0200

    añadio load

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,48 +1,43 @@
 // Importamos el modelo para poder acceder a DB
 var models = require('../models/models.js');
 
+// Autoload - factoriza el código si la ruta incluye :quizId
+// coge el objeto de la BD
+exports.load = function(req, res, next, quizId) {
+	models.Quiz.find(quizId).then(
+		function(quiz){
+			if(quiz){
+				req.quiz = quiz;
+				next();
+			} else { next (new Error('No existe quizId=' + quizId)); }
+		}		
+).catch(function(error){next(error);});
+};
+
 
 // GET /quizes
 exports.index = function(req, res){
 	models.Quiz.findAll().then(function(quizes){
-		res.render('quizes/index.ejs', {quizes: quizes});
-	})
+		res.render('quizes/index', {quizes: quizes});
+	}).catch (function(error){next(error);})
 };
 
 
-// GET /quizes/show
-// Con los métodos models.Quiz.findAll() o find() buscamos los datos en la tabla Quiz y los procesamos en el callback del método success(..)
-// ¡¡ Sustituimos el método success por el método then ('promesa') ya que el método 'success ha quedado anticuado
+// GET /quizes/:id
 exports.show = function(req, res){
-	models.Quiz.find(req.params.quizId).then(function(quiz){
-		res.render('quizes/show', {quiz: quiz})
-	})
+	res.render('quizes/show', {quiz: req.quiz});
+	
 };
 
 
 
-// GET /quizes/answer
-// ¡¡ Sustituimos el método success por el método then ('promesa') ya que el método 'success ha quedado anticuado
+// GET /quizes/:id/answer
 exports.answer = function(req,res){
-	models.Quiz.find(req.params.quizId).then(function(quiz){
-		if(req.query.respuesta === quiz.respuesta) {
-			res.render('quizes/answer', {quiz: quiz, respuesta: 'Correcto'});
-		}else{
-			res.render('quizes/answer', {quiz: quiz, respuesta: 'Incorrecto'});
-		}
-	})
-};
+	var resultado = "Incorrecto";	
+	if(req.query.respuesta === req.quiz.respuesta) {
+			resultado = 'Correcto';
+	}
+	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+		};
 
-/*
-// GET /quizes/question
-exports.question = function(req, res) {
-	res.render('quizes/question', {pregunta: 'Capital de Italia'});
-};
 
-// GET /quizes/answer
-exports.answer = function (req, res) {
-	if (req.query.respuesta === 'Roma'){
-		res.render('quizes/answer', {respuesta: 'Correcto'});}
-	else {res.render('quizes/answer', {respuesta: 'Incorrecto'})}
-};
-*/
